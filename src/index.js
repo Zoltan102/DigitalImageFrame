a9eca7936deb901a1265c2b883a4e085fc585eb1
Zoli <suveges.zoltan@petrik.hu>
2022-09-28 13:01:28 +0200

    működés

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import './style.css';
 
+
 function getImg(){
-    document.getElementById("img").src = url(document.getElementById("url").value);
+    document.getElementById("img").src = document.getElementById("url").value;
 }
 function changeImgWidth(){
-
+    document.getElementById("img").width = document.getElementById("width").value
 }
 function changeBorderWidth(){
-    document.getElementById("img").style.borderWidth = document.getElementById("width").value;
+    document.getElementById("img").style.borderWidth = document.getElementById("border").value + "px";
 }
 function changeBorderColour(){
-
+    document.getElementById("img").style.borderColor = document.getElementById("colour").value;
 }
 
-//document.getElementById("url").addEventListener('change', getImg);
-document.getElementById("border").addEventListener('change', changeBorderWidth);
+document.getElementById("url").addEventListener('change', getImg);
+document.getElementById("width").addEventListener('input', changeImgWidth);
+document.getElementById("border").addEventListener('input', changeBorderWidth);
+document.getElementById("colour").addEventListener('input', changeBorderColour);
