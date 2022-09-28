a76b35324fb6d193a324f777aeedc4a3c876c6b8
Zoli <suveges.zoltan@petrik.hu>
2022-09-28 12:42:14 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css';
 
+function getImg(){
+    document.getElementById("img").src = url(document.getElementById("url").value);
+}
+function changeImgWidth(){
+
+}
+function changeBorderWidth(){
+    document.getElementById("img").style.borderWidth = document.getElementById("width").value;
+}
+function changeBorderColour(){
+
+}
+
+//document.getElementById("url").addEventListener('change', getImg);
+document.getElementById("border").addEventListener('change', changeBorderWidth);
